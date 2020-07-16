302b454502300405227d0528db6167d85ed8635f
Dong Xia <dong7777xia@gmail.com>
2020-07-16 19:18:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,6 @@ end
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  number = :id 
-  gen = {:id => number}
-  gen[number]
+  gen = {:id_hash_generator(number) => number}
+  
 end
